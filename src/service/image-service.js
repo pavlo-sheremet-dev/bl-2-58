eb984f5c9ec016e0df0c19908f43ca24304b10b2
vladyslavketov <106628608+vladyslavketov@users.noreply.github.com>
2023-01-29 11:32:57 +0200

    upd

--- src/service/image-service.js
+++ src/service/image-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '';
+const API_KEY = '563492ad6f91700001000001639cabac73c74accb94b7bf7858095a0';
 axios.defaults.baseURL = 'https://api.pexels.com/v1/';
 axios.defaults.headers.common['Authorization'] = API_KEY;
 axios.defaults.params = {
